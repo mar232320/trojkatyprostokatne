e1bcff69f40167b7e1f62068d31e2dec52136559
mar232320 <60986110+mar232320@users.noreply.github.com>
2021-04-15 09:32:24 +0200

    Add files via upload

---
+++ zapis.py
@@ -0,0 +1,17 @@
+import math
+import itertools
+
+boki = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+trojkaty = itertools.product(boki, boki, boki)
+i = 0
+
+file = open("wynik.txt", "a+")
+
+for trojkat in trojkaty:  
+    if math.pow(trojkat[0], 2) + math.pow(trojkat[1], 2) == math.pow(trojkat[2], 2) and trojkat[1] > trojkat[0]:
+        print(trojkat)
+        file.write(str(trojkat) + "\n")
+        i = i + 1
+        
+file.close()
+print(i)
